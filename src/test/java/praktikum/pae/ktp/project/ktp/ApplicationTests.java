131c8a68eacf9aab16fc6e49594de989352af5d5
whildan fajar <whildanfajarhidayat93@gmail.com>
2022-04-08 14:16:03 +0700

    first commit

---
+++ src/test/java/praktikum/pae/ktp/project/ktp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package praktikum.pae.ktp.project.ktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
